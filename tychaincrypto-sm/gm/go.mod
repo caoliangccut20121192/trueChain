e1a985a667cabccb8dd7e99ebf6b2b55aea54071
caoliangccut20121192 <542080720@qq.com>
2020-03-09 20:38:57 +0800

    国密算法封装

---
+++ tychaincrypto-sm/gm/go.mod
@@ -0,0 +1,14 @@
+//module github.com/ZZMarquis/gm
+
+//go 1.12
+
+//module github.com/truechain/truechain-engineering-code/gm
+
+go 1.13
+
+require (
+	github.com/ZZMarquis/gm v1.1.0
+	github.com/ethereum/go-ethereum v1.9.11
+)
+
+module github.com/tychaincrypto-sm/gm
---
+++ tychaincrypto-sm/gm/sm2/packing_sm2.go
@@ -0,0 +1,43 @@
+package sm2
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+)
+
+//sm2加密算法
+func Encrypt_C1C2C3(src []byte) (error, []byte) {
+	//src := []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	priv, pub, err := GenerateKey(rand.Reader)
+	if err != nil {
+		log.Error(err.Error())
+		return err, nil
+	}
+
+	fmt.Printf("d:%s\n", hex.EncodeToString(priv.D.Bytes()))
+	fmt.Printf("x:%s\n", hex.EncodeToString(pub.X.Bytes()))
+	fmt.Printf("y:%s\n", hex.EncodeToString(pub.Y.Bytes()))
+
+	cipherText, err := Encrypt(pub, src, C1C2C3)
+	if err != nil {
+		log.Error(err.Error())
+		return err, nil
+	}
+	fmt.Printf("cipher text:%s\n", hex.EncodeToString(cipherText))
+	return nil, cipherText
+	//plainText, err := Decrypt(priv, cipherText, C1C2C3)
+	//if err != nil {
+	//	t.Error(err.Error())
+	//	return
+	//}
+	//fmt.Printf("plain text:%s\n", hex.EncodeToString(plainText))
+	//
+	//if !bytes.Equal(plainText, src) {
+	//	t.Error("decrypt result not equal expected")
+	//	return
+	//}
+}
+func Decrypt_C1C2C3(src []byte) (error, []byte) {
+
+}
